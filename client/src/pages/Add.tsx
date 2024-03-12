f9d0cd8d29336271e41d983f78d096a7da06d135
idrissou <idrissoufaysal93@gmail.com>
2024-03-12 17:42:43 +0000

    DDd

--- client/src/pages/Add.tsx
+++ client/src/pages/Add.tsx
@@ -1,8 +1,30 @@
+import { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
 
 export default function Add() {
+  const [value, setValue] = useState('');
+
   return (
-    <div>
-      Add
+    <div className="add">
+      <div className="content">
+        <input type="text"  placeholder="title"/>
+        <div className="editorContainer">
+        <ReactQuill className='editor' theme="snow" value={value} onChange={setValue} />
+        </div>
+      </div>
+      <div className="menu">
+        <div className="file">
+            <input type="file" name="file" id="file" />
+          <label htmlFor="file">
+            Telecharger une image
+          </label>
+        </div>
+        <div className="cat">
+d
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
 
 
 type Post = {
-
+  id:number |undefined
   img: string;
   title: string;
   desc: string;
@@ -21,8 +21,9 @@ type User = {
 };
 
 export default function Home() {
-  const userPosts = [
+  const userPosts:Post[] = [
     {
+      id:1,
       img: img1,
       title: "Titre du post 1",
       desc: "Description du post 1",
@@ -33,6 +34,7 @@ export default function Home() {
       date: new Date(),
     },
     {
+      id:2,
       img: img1,
       title: "Titre du post 1",
       desc: "Description du post 1",
@@ -43,6 +45,7 @@ export default function Home() {
       date: new Date(),
     },
     {
+      id:3,
       img: img1,
       title: "Titre du post 1",
       desc: "Description du post 1",
@@ -53,6 +56,7 @@ export default function Home() {
       date: new Date(),
     },
     {
+      id:4,
       img: img1,
       title: "Titre du post 1",
       desc: "Description du post 1",
@@ -63,6 +67,7 @@ export default function Home() {
       date: new Date(),
     },
     {
+      id:5,
       img: img1,
       title: "Titre du post 1",
       desc: "Description du post 1",
@@ -73,6 +78,7 @@ export default function Home() {
       date: new Date(),
     },
     {
+      id:6,
       img: img2,
       title: "Titre du post 2",
       desc: "Description du post 2",
@@ -88,16 +94,16 @@ export default function Home() {
   return (
     <div className="posts">
      {userPosts.map((post: Post) => (
-        <div key={post.title}>
+        <div key={post.id}>
           <div className="post">
             <Link to='/user/:id' className="links">
 
             <div className="userInfo">
               <img className="img" src={post.user?.img} alt={post.user?.username} />
-              <h3>{post.user?.username}</h3>
+              <span>{post.user?.username}</span>
             </div>
             </Link>
-            <Link to='/desc' className="links">
+            <Link to={`/post/${post.id}`} className="links">
 
             <div className="postContent">
               <img src={post.img} alt="" />
